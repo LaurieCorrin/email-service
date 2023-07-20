29f46f2229557324ee818eb27ec08e08d0790c60
Laurie Corrin <laurie.corrin@multiverse.io>
2023-07-20 11:31:41 -0700

    trigger redeploy

--- handler.js
+++ handler.js
@@ -5,7 +5,7 @@ module.exports.sendMail = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+        message: 'Go Serverless! Your function executed successfully!',
         input: event,
       },
       null,
